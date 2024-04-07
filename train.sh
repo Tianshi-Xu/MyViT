5cf7d6afb740efad1e2c65e360255a08e17f8db4
tianshi-xu <994876761@qq.com>
2024-04-07 19:28:11 +0800

    .

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@
 # CUDA_VISIBLE_DEVICES=2 python train.py -c configs/datasets/cifar100.yml --model vit_7_4_32_c100 /home/xts/code/dataset/cifar100/
 
 # pretrain tiny
-# CUDA_VISIBLE_DEVICES=4 python train.py -c configs/datasets/tiny_imagenet.yml --model vit_9_12_64 /home/xts/code/dataset/tiny-imagenet-200
+# CUDA_VISIBLE_DEVICES=2 python train.py -c configs/datasets/ViT/tiny.yml --model vit_9_12_64 /home/xts/code/dataset/tiny-imagenet-200
 
 # nas
 CUDA_VISIBLE_DEVICES=4 python train_nas.py -c configs/datasets/ViT/cifar10_nas.yml --model vit_7_4_32 /home/xts/code/dataset/cifar10/
